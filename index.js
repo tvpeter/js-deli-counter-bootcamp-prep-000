e2ed20dd40142f6bd6343520feff40f8e2689929
peter <withtvpeter@gmail.com>
2018-09-30 13:40:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeliLine = [];
+var num = katzDeliLine.length;
+
+function takeANumber(num, name)
+{
+  katzDeliLine.push(name);
+  
+  console.log(`Welcome, ${name}. You are number ${num} in line.`);
+  
+}
