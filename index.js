7e35cb6788628b3c848894d76660dafa8bda4ded
peter <withtvpeter@gmail.com>
2018-09-30 13:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine (katzDeliLine)
 {
   if(katzDeliLine.length === 0)
   {
-    return "The line is currently empty";
+    return "The line is currently empty.";
   }
   var stmt = "The line is currently: ";
   for(var i=0; i < katzDeliLine.length; i++)
