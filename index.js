68815c5dd1002638aea72ade566a177a023010ab
peter <withtvpeter@gmail.com>
2018-09-30 13:40:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 var katzDeliLine = [];
-var num = katzDeliLine.length;
 
-function takeANumber(num, name)
+
+
+function takeANumber(katzDeliLine, name)
 {
   katzDeliLine.push(name);
   
-  console.log(`Welcome, ${name}. You are number ${num} in line.`);
+  console.log(`Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`);
   
+}
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine.length ===  0)
+  {
+    return "There is nobody waiting to be served!";
+  
+  }
+  var cp = katzDeliLine[0];
+  
+  katzDeliLine.shift();
+  
+  return `Currently serving ${cp}.`;
+  
+}
+
+function currentLine (katzDeliLine)
+{
+  if(katzDeliLine.length === 0)
+  {
+    return "The line is currently empty";
+  }
+  var stmt = "The line is currently: ";
+  for(var i=0; i < katzDeliLine.length; i++)
+  {
+    stmt += `${i+1}. ${katzDeliLine[i]}`;
+  }
+  
+  return stmt;
+
 }
